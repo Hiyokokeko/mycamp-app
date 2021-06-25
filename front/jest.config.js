b453b1d0d6b9c5a6e790b0fc122f53042fb13e13
Hiyokokeko <sukepon0914@gmail.com>
2021-06-25 10:55:45 +0900

    [add]jestで仮テスト追加

---
+++ front/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "vue"],
+  transform: {
+    ".*\\.(vue)$": "vue-jest",
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+    "^~/(.*)$": "<rootDir>/$1",
+  },
+}
