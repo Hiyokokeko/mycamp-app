f5843bad781ff87cd92c664fb1fcce63b308df40
Hiyokokeko <sukepon0914@gmail.com>
2021-07-05 16:20:54 +0900

    [add]iam_role, s3の定義

---
+++ terraform/s3.tf
@@ -0,0 +1,22 @@
+# パブリックバケット
+resource "aws_s3_bucket" "mycamp-s3-bucket" {
+  bucket = "mycamp-s3-bucket-for-images"
+  acl    = "public-read"
+
+  tags = {
+    Name = "mycamp-s3-bucket-for-images"
+  }
+}
+
+# ログバケット
+resource "aws_s3_bucket" "mycamp_alb_log" {
+  bucket = "artifact-terraform-for-mycamp"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
